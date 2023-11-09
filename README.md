be9aa5263e2ce156076a729efc7fbc8adbeabcfb
Gaurav Gupta <gouravgupta840@gmail.com>
2023-11-09 23:37:57 +0530

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+## Docker Prom-Client Express
+
+-   docker build -t gupta840/express-prom-client .
+-   docker push gupta840/express-prom-client
+
+1.  Create account in render.com
+2.  Deploy the docker image
---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const promClient = require('prom-client');
+
+// Create a Prometheus registry
+const registry = new promClient.Registry();
+
+// Create a counter metric
+const counter = new promClient.Counter({
+  name: 'example_counter',
+  help: 'A simple example counter metric',
+  registers: [registry],
+});
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "hello Gaurav",
+  });
+});
+
+// Expose a route to increment the counter
+app.get('/increment', (req, res) => {
+  counter.inc();
+  res.send('Counter incremented!');
+});
+
+// Expose Prometheus metrics endpoint
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', registry.contentType);
+  res.end(await registry.metrics());
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("App is running");
+});
